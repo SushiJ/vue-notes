d08479dbea80fd1328bc7dd7f323a2f2f0d07b21
Sushij <sushant.wurk@gmail.com>
2023-04-10 15:09:15 +0530

    Feat. Added Chi router for better routing

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,23 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP port")
 	flag.Parse()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/check", checkHealth)
+	r := chi.NewRouter()
+	r.Get("/check", checkHealth)
 
-	// View Notes
-	mux.HandleFunc("/notes/view", viewNotes)
-	// Create notes
-	mux.HandleFunc("/notes/create", createNotes)
+	r.Route("/notes", func(r chi.Router) {
+		r.Get("/", getNotes)
+		r.Get("/{id}", getNotesById)
+		r.Post("/create", createNotes)
+	})
 
 	log.Printf("Live at http://localhost%s", *addr)
-	log.Fatal((http.ListenAndServe(*addr, mux)))
+	log.Fatal((http.ListenAndServe(*addr, r)))
+}
+
+type Notes struct {
+	Id        string    `json:"id"`
+	Title     string    `json:"title"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Content   string    `json:"content"`
+}
+
+func checkHealth(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("200"))
+}
+
+var notes = []Notes{
+	{Id: "1", Title: "First note", Content: "This is the first Note", CreatedAt: time.Now(), UpdatedAt: time.Now()},
+	{Id: "2", Title: "Second note", Content: "This is the second Note", CreatedAt: time.Now(), UpdatedAt: time.Now()},
+}
+
+func getNotes(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(notes)
+}
+
+func getNotesById(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "id")
+	fmt.Fprintf(w, "Id: %s", idParam)
+}
+
+func createNotes(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allow", http.StatusMethodNotAllowed)
+	}
 }
